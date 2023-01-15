7ae22e080ba3773505b20cda6dd1eff37f673813
prisar <prisar230@gmail.com>
2023-01-15 14:43:45 +0530

    a

--- kabita/ContentView.swift
+++ kabita/ContentView.swift
@@ -16,19 +16,48 @@ struct ContentView: View {
     
     var body: some View {
         Text(joke)
-                Button {
-                    Task {
-                        let (data, _) = try await URLSession.shared.data(from: URL(string:"https://api.chucknorris.io/jokes/random")!)
-                        let decodedResponse = try? JSONDecoder().decode(Joke.self, from: data)
-                        joke = decodedResponse?.value ?? ""
-                    }
-                } label: {
-                    Text("Fetch Joke")
+        Button {
+            Task {
+                let (data, _) = try await URLSession.shared.data(from: URL(string:"https://api.chucknorris.io/jokes/random")!)
+                let decodedResponse = try? JSONDecoder().decode(Joke.self, from: data)
+                joke = decodedResponse?.value ?? ""
+            }
+        } label: {
+            Text("Fetch Joke")
+        }
+       
+        VStack {
+            HStack {
+                Image("house-mod-2")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                VStack(alignment: .leading) {
+                    Text("SwiftUI")
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                    Text("Drawing a Border with Rounded Corners")
+                        .font(.title)
+                        .fontWeight(.black)
+                        .foregroundColor(.primary)
+                        .lineLimit(3)
+                    Text("Written by Simon Ng blog".uppercased())
+                        .font(.caption)
+                        .foregroundColor(.secondary)
                 }
+                .layoutPriority(100)
+                
+                Spacer()
+            }
+            .padding()
+        }
+        .cornerRadius(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color(.sRGB, red: 150/255, green: 150/255, blue: 150/255, opacity: 0.1), lineWidth: 1)
+                )
+                .padding([.top, .horizontal])
     }
-                               
-                               
-                               
 }
 
 struct ContentView_Previews: PreviewProvider {
